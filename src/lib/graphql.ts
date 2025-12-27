3112549ce5e5c74374daed6d05d3a067860644f9
Gérald Leban <gerald.leban0@gmail.com>
2025-12-27 21:49:01 +0100

    chore: mise en place du typage des requetes graphql

---
+++ src/lib/graphql.ts
@@ -0,0 +1,9 @@
+import { initGraphQLTada } from 'gql.tada';
+import type { introspection } from '../graphql-env';
+
+export const graphql = initGraphQLTada<{
+	introspection: introspection;
+}>();
+
+export { readFragment } from 'gql.tada';
+export type { FragmentOf, ResultOf, TadaDocumentNode, VariablesOf } from 'gql.tada';
