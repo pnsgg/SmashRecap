ef2f4dfe84f8d60d2a92eea68ae5e817d9b2eb4a
Gérald Leban <gerald.leban0@gmail.com>
2025-12-28 20:23:38 +0100

    feat: charge la font variable du coté de remotion

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,9 +1,25 @@
-import { Composition } from 'remotion';
+import { loadFont } from '@remotion/fonts';
+import React from 'react';
+import { Composition, staticFile } from 'remotion';
 import { Main } from './Main';
+import { FPS, MAIN_COMPOSITION_HEIGHT, MAIN_COMPOSITION_WIDTH } from './config';
+
+import { ThisIsMyRecap, thisIsMyRecapSchema } from './ThisIsMyRecap';
+
+import { typography } from './styles';
+import './styles/remotion.css';
 
 // Each <Composition> is an entry in the sidebar!
 
 export const RemotionRoot: React.FC = () => {
+  loadFont({
+    family: typography.fontFamily,
+    url: staticFile('/fonts/InstrumentSans-VariableFont_wdth,wght.ttf'),
+    display: 'block'
+  })
+    .then(() => console.log('Font loaded'))
+    .catch(console.error);
+
   return (
     <>
       {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
@@ -13,11 +29,31 @@ export const RemotionRoot: React.FC = () => {
         id="Main"
         component={Main}
         durationInFrames={150}
-        fps={30}
-        width={1920}
-        height={1080}
+        fps={FPS}
+        width={MAIN_COMPOSITION_WIDTH}
+        height={MAIN_COMPOSITION_HEIGHT}
         // You can override these props for each render:
         // https://www.remotion.dev/docs/parametrized-rendering
+      />
+      <Composition
+        id="ThisIsMyRecap"
+        component={ThisIsMyRecap}
+        schema={thisIsMyRecapSchema}
+        durationInFrames={FPS}
+        width={MAIN_COMPOSITION_WIDTH}
+        height={MAIN_COMPOSITION_HEIGHT}
+        fps={FPS}
+        defaultProps={{
+          background: true,
+          year: 2025,
+          user: {
+            prefix: 'PNS',
+            gamerTag: 'RouxChov',
+            country: 'France',
+            pronouns: 'He/Him',
+            socialMedias: {}
+          }
+        }}
       />
     </>
   );
