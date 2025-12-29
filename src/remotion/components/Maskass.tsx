b1ade5cf55eeb67947c1e48b809404e7c4f62dda
Gérald Leban <gerald.leban0@gmail.com>
2025-12-29 21:29:55 +0100

    feat: ajoute maskass

---
+++ src/remotion/components/Maskass.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Img, staticFile, useCurrentFrame, useVideoConfig } from 'remotion';
+
+const SPRITES = [
+  '/images/maskass/fly-0.png',
+  '/images/maskass/fly-1.png',
+  '/images/maskass/fly-2.png',
+  '/images/maskass/fly-3.png'
+];
+
+interface MaskassProps {
+  style?: React.CSSProperties;
+  fps?: number;
+}
+
+export const Maskass: React.FC<MaskassProps> = ({ style, fps = 24 }) => {
+  const frame = useCurrentFrame();
+  const { fps: videoFps } = useVideoConfig();
+
+  const timeInSeconds = frame / videoFps;
+  const currentAnimationFrame = Math.floor(timeInSeconds * fps);
+  const spriteIndex = currentAnimationFrame % SPRITES.length;
+
+  const currentSprite = SPRITES[spriteIndex];
+
+  return (
+    <Img
+      src={staticFile(currentSprite)}
+      style={{
+        display: 'block',
+        objectFit: 'contain',
+        ...style
+      }}
+    />
+  );
+};
