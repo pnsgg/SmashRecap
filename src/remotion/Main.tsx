a583dda2e13cdc9fb3c541bcaa56abcb837cfea3
Gérald Leban <gerald.leban0@gmail.com>
2025-12-31 15:33:20 +0100

    refactor: anime les séquence de la composition finale

--- src/remotion/Main.tsx
+++ src/remotion/Main.tsx
@@ -1,25 +1,71 @@
 import React from 'react';
-import { Series } from 'remotion';
+import { AbsoluteFill, interpolate, interpolateColors, Sequence, useCurrentFrame } from 'remotion';
+import {
+  END_CARD_DURATION,
+  FPS,
+  PERFORMANCES_DURATION,
+  THIS_IS_MY_RECAP_DURATION,
+  TOURNAMENTS_DURATION
+} from './config';
 import { EndCard } from './EndCard';
+import { ATTENDANCE, ME, PERFORMANCES, YEAR } from './mock';
+import { MyPerformances } from './MyPerformances';
+import { colors } from './styles';
 import { ThisIsMyRecap } from './ThisIsMyRecap';
 import { Tournaments } from './Tournaments';
-import { END_CARD_DURATION, THIS_IS_MY_RECAP_DURATION, TOURNAMENTS_DURATION } from './config';
-import { ATTENDANCE, ME, YEAR } from './mock';
 
 export const Main: React.FC = () => {
+  const frame = useCurrentFrame();
+
+  const backgroundColor = interpolateColors(
+    frame,
+    [THIS_IS_MY_RECAP_DURATION, THIS_IS_MY_RECAP_DURATION + FPS / 2],
+    [colors.nearlyBlack, colors.reallyWhite]
+  );
+
+  const blur = interpolate(
+    frame,
+    [
+      THIS_IS_MY_RECAP_DURATION + TOURNAMENTS_DURATION - FPS,
+      THIS_IS_MY_RECAP_DURATION + TOURNAMENTS_DURATION
+    ],
+    [0, 10],
+    {
+      extrapolateRight: 'clamp'
+    }
+  );
+
   return (
-    <>
-      <Series>
-        <Series.Sequence name="ThisIsMyRecap" durationInFrames={THIS_IS_MY_RECAP_DURATION}>
-          <ThisIsMyRecap user={ME} year={YEAR} />
-        </Series.Sequence>
-        <Series.Sequence name="Tournaments" durationInFrames={TOURNAMENTS_DURATION}>
+    <AbsoluteFill style={{ backgroundColor }}>
+      <Sequence name="ThisIsMyRecap" durationInFrames={THIS_IS_MY_RECAP_DURATION}>
+        <ThisIsMyRecap user={ME} year={YEAR} />
+      </Sequence>
+
+      <Sequence
+        name="Tournaments"
+        from={THIS_IS_MY_RECAP_DURATION}
+        durationInFrames={TOURNAMENTS_DURATION + PERFORMANCES_DURATION}
+      >
+        <div style={{ filter: `blur(${blur}px)`, width: '100%', height: '100%' }}>
           <Tournaments attendance={ATTENDANCE} year={YEAR} />
-        </Series.Sequence>
-        <Series.Sequence name="EndCard" durationInFrames={END_CARD_DURATION}>
-          <EndCard />
-        </Series.Sequence>
-      </Series>
-    </>
+        </div>
+      </Sequence>
+
+      <Sequence
+        name="Performances"
+        from={THIS_IS_MY_RECAP_DURATION + TOURNAMENTS_DURATION}
+        durationInFrames={PERFORMANCES_DURATION}
+      >
+        <MyPerformances performances={PERFORMANCES} />
+      </Sequence>
+
+      <Sequence
+        name="EndCard"
+        from={THIS_IS_MY_RECAP_DURATION + TOURNAMENTS_DURATION + PERFORMANCES_DURATION}
+        durationInFrames={END_CARD_DURATION}
+      >
+        <EndCard />
+      </Sequence>
+    </AbsoluteFill>
   );
 };
