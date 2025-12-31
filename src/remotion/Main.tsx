26a66590c79075f0618c8ca38737b551bdb06fa1
Gérald Leban <gerald.leban0@gmail.com>
2025-12-31 11:50:15 +0100

    feat: ajoute les séquences dans la composition principale

--- src/remotion/Main.tsx
+++ src/remotion/Main.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Series } from 'remotion';
+import { EndCard } from './EndCard';
 import { ThisIsMyRecap } from './ThisIsMyRecap';
-import { THIS_IS_MY_RECAP_DURATION } from './config';
-import { ME, YEAR } from './mock';
+import { Tournaments } from './Tournaments';
+import { END_CARD_DURATION, THIS_IS_MY_RECAP_DURATION, TOURNAMENTS_DURATION } from './config';
+import { ATTENDANCE, ME, YEAR } from './mock';
 
 export const Main: React.FC = () => {
   return (
     <>
       <Series>
-        <Series.Sequence durationInFrames={THIS_IS_MY_RECAP_DURATION}>
+        <Series.Sequence name="ThisIsMyRecap" durationInFrames={THIS_IS_MY_RECAP_DURATION}>
           <ThisIsMyRecap user={ME} year={YEAR} />
+        </Series.Sequence>
+        <Series.Sequence name="Tournaments" durationInFrames={TOURNAMENTS_DURATION}>
+          <Tournaments attendance={ATTENDANCE} year={YEAR} />
+        </Series.Sequence>
+        <Series.Sequence name="EndCard" durationInFrames={END_CARD_DURATION}>
+          <EndCard />
         </Series.Sequence>
       </Series>
     </>
