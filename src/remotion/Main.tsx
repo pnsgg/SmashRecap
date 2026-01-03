4e831515ce3019958ffd4fde8f465abf81d82f86
Gérald Leban <gerald.leban0@gmail.com>
2026-01-03 15:34:12 +0100

    refactor: ajoute le schéma de validation de la composition principale

--- src/remotion/Main.tsx
+++ src/remotion/Main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AbsoluteFill, interpolate, interpolateColors, Sequence, useCurrentFrame } from 'remotion';
+import { z } from 'zod';
 import {
   END_CARD_DURATION,
   FAVOURITE_CHARACTER_DURATION,
@@ -10,25 +11,26 @@ import {
 } from './config';
 import { EndCard } from './EndCard';
 import { FavouriteCharacters } from './FavouriteCharacter';
-import { ATTENDANCE, FAVOURITE_CHARACTERS, ME, PERFORMANCES, YEAR } from './mock';
+import { ATTENDANCE, FAVOURITE_CHARACTERS, PERFORMANCES, YEAR } from './mock';
 import { MyPerformances } from './MyPerformances';
 import { colors } from './styles';
-import { ThisIsMyRecap } from './ThisIsMyRecap';
+import { ThisIsMyRecap, thisIsMyRecapSchema } from './ThisIsMyRecap';
 import { Tournaments } from './Tournaments';
 
-export const Main: React.FC = () => {
+export const mainSchema = z.object({
+  thisIsMyRecap: thisIsMyRecapSchema
+});
+
+export type MainProps = z.infer<typeof mainSchema>;
+
+export const Main: React.FC<MainProps> = ({ thisIsMyRecap: { user, year } }) => {
   const frame = useCurrentFrame();
 
   const favStart = THIS_IS_MY_RECAP_DURATION + TOURNAMENTS_DURATION + PERFORMANCES_DURATION;
 
   const backgroundColor = interpolateColors(
     frame,
-    [
-      THIS_IS_MY_RECAP_DURATION,
-      THIS_IS_MY_RECAP_DURATION + FPS / 2,
-      favStart - FPS / 2,
-      favStart
-    ],
+    [THIS_IS_MY_RECAP_DURATION, THIS_IS_MY_RECAP_DURATION + FPS / 2, favStart - FPS / 2, favStart],
     [colors.nearlyBlack, colors.reallyWhite, colors.reallyWhite, colors.nearlyBlack]
   );
 
@@ -47,7 +49,7 @@ export const Main: React.FC = () => {
   return (
     <AbsoluteFill style={{ backgroundColor }}>
       <Sequence name="ThisIsMyRecap" durationInFrames={THIS_IS_MY_RECAP_DURATION}>
-        <ThisIsMyRecap user={ME} year={YEAR} />
+        <ThisIsMyRecap user={user} year={year} />
       </Sequence>
 
       <Sequence
