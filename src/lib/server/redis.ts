4763130fa71a7f2daa6c2cf8109eff57d493537e
Gérald Lbn <gerald.leban0@gmail.com>
2026-01-31 21:02:13 +0100

    feat: add helper function to create keys for generated image url

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -13,10 +13,19 @@ export const makeRecapUrlKey = (year: number, userId: number): string =>
   `recap:url:${year}:${userId}`;
 
 /**
+ * Creates a key pointing to a still image URL for the user's recap for a given year
+ * @param year year of the recap to get
+ * @param userId start.gg id of the player
+ * @returns A redis key pointing to a recap still image URL for the user for a given year
+ */
+export const makeStillUrlKey = (year: number, userId: number): string =>
+  `recap:still:url:${year}:${userId}`;
+
+/**
  * Creates a key pointing to stats for a user in a given year
  * @param year year of the recap to get
  * @param userId start.gg id of the player
  * @returns A redis key pointing to stats for a given player in a given year
  */
-export const makerRecapStatsKey = (year: number, userId: number | '*'): string =>
+export const makeRecapStatsKey = (year: number, userId: number | '*'): string =>
   `recap:stats:${year}:${userId}`;
