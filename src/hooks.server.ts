6580b69c0a9472c7df1f6b9c4cadfa0ebadadc53
Gérald Lbn <gerald.leban0@gmail.com>
2026-01-31 13:17:01 +0100

    chore: add @inlang/paraglide-js

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,7 @@
-import { rateLimitter } from '$lib/server/rate-limitter';
+import { sequence } from '@sveltejs/kit/hooks';
 import { type Handle } from '@sveltejs/kit';
+import { rateLimitter } from '$lib/server/rate-limitter';
+import { paraglideMiddleware } from '$lib/paraglide/server';
 
 export const rateLimit: Handle = async ({ event, resolve }) => {
   if (event.url.pathname.startsWith('/api/')) {
@@ -16,4 +18,15 @@ export const rateLimit: Handle = async ({ event, resolve }) => {
   return resolve(event);
 };
 
-export const handle = rateLimit;
+export const i18n: Handle = async ({ event, resolve }) => {
+  return paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+    event.request = localizedRequest;
+    return resolve(event, {
+      transformPageChunk: ({ html }) => {
+        return html.replace('%lang%', locale);
+      }
+    });
+  });
+};
+
+export const handle = sequence(rateLimit, i18n);
